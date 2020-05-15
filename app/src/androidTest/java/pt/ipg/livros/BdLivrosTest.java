661ec3aab015e44194e1669974cac3e926a98a4c
Noel Lopes <noel@ipg.pt>
2020-05-15 10:59:22 +0100

    Adicionados testes para verificar se é possível ler e alterar categorias

--- app/src/androidTest/java/pt/ipg/livros/BdLivrosTest.java
+++ app/src/androidTest/java/pt/ipg/livros/BdLivrosTest.java
@@ -1,6 +1,7 @@
 package pt.ipg.livros;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -53,6 +54,54 @@ public class BdLivrosTest {
 
         long id = tabelaCategorias.insert(Converte.categoriaToContentValues(categoria));
         assertNotEquals(-1, id);
+
+        bdLivros.close();
+    }
+
+    @Test
+    public void consegueLerCategorias() {
+        Context appContext = getTargetContext();
+
+        BdLivrosOpenHelper openHelper = new BdLivrosOpenHelper(appContext);
+        SQLiteDatabase bdLivros = openHelper.getWritableDatabase();
+
+        BdTableCategorias tabelaCategorias = new BdTableCategorias(bdLivros);
+
+        Cursor cursor = tabelaCategorias.query(BdTableCategorias.TODOS_CAMPOS, null, null, null, null, null);
+        int registos = cursor.getCount();
+        cursor.close();
+
+        Categoria categoria = new Categoria();
+        categoria.setDescricao("Sci-fi");
+
+        long id = tabelaCategorias.insert(Converte.categoriaToContentValues(categoria));
+        assertNotEquals(-1, id);
+
+        cursor = tabelaCategorias.query(BdTableCategorias.TODOS_CAMPOS, null, null, null, null, null);
+        assertEquals(registos + 1, cursor.getCount());
+        cursor.close();
+
+        bdLivros.close();
+    }
+
+    @Test
+    public void consegueAtualizarCategorias() {
+        Context appContext = getTargetContext();
+
+        BdLivrosOpenHelper openHelper = new BdLivrosOpenHelper(appContext);
+        SQLiteDatabase bdLivros = openHelper.getWritableDatabase();
+
+        BdTableCategorias tabelaCategorias = new BdTableCategorias(bdLivros);
+
+        Categoria categoria = new Categoria();
+        categoria.setDescricao("Romanc");
+
+        long id = tabelaCategorias.insert(Converte.categoriaToContentValues(categoria));
+        assertNotEquals(-1, id);
+
+        categoria.setDescricao("Romance");
+        int registosAfetados = tabelaCategorias.update(Converte.categoriaToContentValues(categoria), BdTableCategorias._ID + "=?", new String[]{String.valueOf(id)});
+        assertEquals(1, registosAfetados);
 
         bdLivros.close();
     }
